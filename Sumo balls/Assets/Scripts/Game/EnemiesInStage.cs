0b322e0d9d00d23ca342cf679e823d5af23f8216
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-03-04 17:44:36 +0100

    Add description.

--- Sumo balls/Assets/Scripts/Game/EnemiesInStage.cs
+++ Sumo balls/Assets/Scripts/Game/EnemiesInStage.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 [Serializable]
+
 public struct EnemiesInStage 
 {
     public EnemyBelts.Belt belt;
+    [Tooltip("Represents minimum number of enemies spawned in a stage. If during gameplay all enemies have been used, new enemies will be spawned randomly. If value is set to " +
+        "-1 enemies of this type will be spawned indefinitely")]
     public int numberOfEnemies;
 
 }
