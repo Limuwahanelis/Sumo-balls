d98dd070b00a548675f220e25a94131c3bb1b185
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-03-05 19:59:58 +0100

    Made enemy spawner less likely to spawn enemies next to each other, include falling balls into spawn location selection.

--- Sumo balls/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Sumo balls/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -15,11 +17,39 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] AudioPool _audioPool;
     [SerializeField] Transform _playerPos;
     private List<NormalEnemy> _allEnemies = new List<NormalEnemy>();
+    private List<NormalEnemy> _spawnedEnemies= new List<NormalEnemy>();
+    private List<TransformToAvoid> _transformsToAvoidSpawn = new List<TransformToAvoid>();
+    private List<SpawnCandidate> _spawnCandidates = new List<SpawnCandidate>();
+    private struct TransformToAvoid
+    {
+        public Transform transform;
+        public float distance;
+        public bool hasPiority;
+    }
+    private struct SpawnCandidate
+    {
+        public Vector3 position;
+        public List<DistancePriority> distances;
+    }
+    private struct DistancePriority
+    {
+        public float distance;
+        public bool hasPriority;
+    }
+
     public NormalEnemy SpawnEnemy()
     {
         NormalEnemy enemy = _enemyPool.GetEnemy();
         if(!_allEnemies.Contains(enemy)) _allEnemies.Add(enemy);
+        _spawnedEnemies.Add(enemy);
+        enemy.OnDeath.AddListener(OnEnemyDeath);
         enemy.SetRBPos(SelectSpawnPos(enemy.transform.localScale.y));
+        _transformsToAvoidSpawn.Add(new TransformToAvoid()
+        {
+            transform = enemy.Rigidbody.transform,
+            distance=1f,
+            hasPiority=true
+        });
         enemy.Rigidbody.velocity = Vector3.zero;
         enemy.SetPlayer(_playerPos.gameObject);
         enemy.SetAudioPool(_audioPool);
@@ -28,55 +58,114 @@ public class EnemySpawner : MonoBehaviour
     }
     private Vector3 SelectSpawnPos(float enemySacle)
     {
+        _spawnCandidates.Clear();
         float leftXrange = _playerPos.position.x - _playerSafeSpace;
         float upZrange = _playerPos.position.z + _playerSafeSpace;
         float downZrang = _playerPos.position.z - _playerSafeSpace;
         float rightXrange = _playerPos.position.x + _playerSafeSpace;
-        float spawnZ;
+        float spawnZ=-20;
         float spawnX = -10;
-        spawnZ = Random.Range(-_spawnRange, _spawnRange);
-        if (downZrang >= _spawnRange || upZrange <= -_spawnRange) // outside spawn
+        bool isSelectingSpawnPos = true;
+        bool canSpawn = true;
+        int selectiontries = 1;
+        SpawnCandidate winner;
+        while (isSelectingSpawnPos)
         {
-            spawnX = Random.Range(-_spawnRange, _spawnRange);
-        }
-        else
-        {
-            if (leftXrange >= -_spawnRange && rightXrange <= _spawnRange) // inside spawn from both sides
+            canSpawn = true;
+            spawnZ = Random.Range(-_spawnRange, _spawnRange);
+            if (downZrang >= _spawnRange || upZrange <= -_spawnRange) // outside spawn
             {
-
-                if (spawnZ >= downZrang && spawnZ <= upZrange) // z spawn is inside _playerBody so x must be outside
-                {
-                    int a = Random.Range(0, 2); // select which side
-                    if (a == 1) //left
-                    {
-                        spawnX = Random.Range(-_spawnRange, leftXrange);
-                    }
-                    else //right
-                    {
-                        spawnX = Random.Range(rightXrange, _spawnRange);
-                    }
-                }
-                else
-                {
-                    spawnX = Random.Range(-_spawnRange, _spawnRange);
-                }
-
+                spawnX = Random.Range(-_spawnRange, _spawnRange);
             }
             else
             {
-                if (leftXrange <= -_spawnRange) // outside on left
+                if (leftXrange >= -_spawnRange && rightXrange <= _spawnRange) // inside spawn from both sides
                 {
-                    if (spawnZ >= downZrang && spawnZ <= upZrange) spawnX = Random.Range(rightXrange, _spawnRange);
-                    else spawnX = Random.Range(-_spawnRange, _spawnRange);
+
+                    if (spawnZ >= downZrang && spawnZ <= upZrange) // z spawn is inside _playerBody so x must be outside
+                    {
+                        int a = Random.Range(0, 2); // select which side
+                        if (a == 1) //left
+                        {
+                            spawnX = Random.Range(-_spawnRange, leftXrange);
+                        }
+                        else //right
+                        {
+                            spawnX = Random.Range(rightXrange, _spawnRange);
+                        }
+                    }
+                    else
+                    {
+                        spawnX = Random.Range(-_spawnRange, _spawnRange);
+                    }
+
                 }
-                else if (rightXrange >= _spawnRange) // outside on right
+                else
                 {
-                    if (spawnZ >= downZrang && spawnZ <= upZrange) spawnX = Random.Range(-_spawnRange, leftXrange);
-                    else spawnX = Random.Range(-_spawnRange, _spawnRange);
+                    if (leftXrange <= -_spawnRange) // outside on left
+                    {
+                        if (spawnZ >= downZrang && spawnZ <= upZrange) spawnX = Random.Range(rightXrange, _spawnRange);
+                        else spawnX = Random.Range(-_spawnRange, _spawnRange);
+                    }
+                    else if (rightXrange >= _spawnRange) // outside on right
+                    {
+                        if (spawnZ >= downZrang && spawnZ <= upZrange) spawnX = Random.Range(-_spawnRange, leftXrange);
+                        else spawnX = Random.Range(-_spawnRange, _spawnRange);
+                    }
                 }
             }
+            SpawnCandidate candidate = new SpawnCandidate()
+            {
+                position = new Vector3(spawnX, _arenaYpos * enemySacle, spawnZ),
+                distances = new List<DistancePriority>()
+            };
+            _spawnCandidates.Add(candidate);
+            for (int i=0;i<_transformsToAvoidSpawn.Count;i++)
+            {
+                TransformToAvoid toCheck = _transformsToAvoidSpawn[i];
+                Vector3 checkVec = new Vector3(toCheck.transform.position.x, _arenaYpos * enemySacle, toCheck.transform.position.z);
+                if(math.pow(candidate.position.x-toCheck.transform.position.x,2)+ math.pow(candidate.position.z - toCheck.transform.position.z, 2) <= math.pow(toCheck.distance + enemySacle, 2))
+                {
+                    canSpawn = false;
+                }
+                candidate.distances.Add(new DistancePriority()
+                {
+                    distance = Vector3.Distance(checkVec, candidate.position),
+                    hasPriority = toCheck.hasPiority
+                });
+            }
+            if (!canSpawn)
+            {
+                selectiontries++;
+                if (selectiontries == 5) isSelectingSpawnPos = false;
+            }
+            else isSelectingSpawnPos = false;
         }
-        Vector3 randomPos = new Vector3(spawnX, _arenaYpos* enemySacle, spawnZ);
+       if (canSpawn)
+        {
+            return new Vector3(_spawnCandidates[_spawnCandidates.Count - 1].position.x, _arenaYpos * enemySacle, _spawnCandidates[_spawnCandidates.Count - 1].position.z);
+        }
+        else
+        {
+            winner = _spawnCandidates[0];
+            winner.distances.Sort(delegate (DistancePriority x, DistancePriority y)
+            {
+                if (x.distance > y.distance) return 1;
+                else if (x.distance < y.distance) return -1;
+                else return 0;
+            });
+            for (int i = 1; i < _spawnCandidates.Count; i++)
+            {
+                _spawnCandidates[i].distances.Sort(delegate (DistancePriority x, DistancePriority y)
+                {
+                    if (x.distance > y.distance) return 1;
+                    else if (x.distance < y.distance) return -1;
+                    else return 0;
+                });
+                if (_spawnCandidates[i].distances[0].distance > winner.distances[0].distance && !_spawnCandidates[i].distances[0].hasPriority) winner = _spawnCandidates[i];
+            }
+        }
+        Vector3 randomPos = new Vector3(winner.position.x, _arenaYpos* enemySacle, winner.position.z);
         return randomPos;
     }
     public void ReturnAllEnemiesToPool()
@@ -88,7 +177,8 @@ public class EnemySpawner : MonoBehaviour
             if(enemy.gameObject.activeSelf) enemy.ReturnToPool();
 
         }
-        
+        _transformsToAvoidSpawn.Clear();
+        _spawnedEnemies.Clear();
     }
     public void SetAllEnemyScript(bool value)
     {
@@ -96,6 +186,31 @@ public class EnemySpawner : MonoBehaviour
         {
             enemy.enabled = value;
         }
+    }
+    public void UpdateAvoidTransforms(Transform trans,float distance,bool remove)
+    {
+        if(remove)
+        {
+            TransformToAvoid tr= _transformsToAvoidSpawn.Find((x) => x.transform == trans);
+            _transformsToAvoidSpawn.Remove(tr);
+        }
+        else
+        {
+            TransformToAvoid tr = new TransformToAvoid()
+            {
+                transform = trans,
+                distance = distance,
+                hasPiority = false
+            };
+            _transformsToAvoidSpawn.Add(tr);
+        }
+    }
+    private void OnEnemyDeath(Enemy en)
+    {
+        en.OnDeath.RemoveListener(OnEnemyDeath);
+        _spawnedEnemies.Remove(en as NormalEnemy);
+        TransformToAvoid tr= _transformsToAvoidSpawn.Find((x) => x.transform == (en as NormalEnemy).Rigidbody.transform);
+        _transformsToAvoidSpawn.Remove(tr);
     }
     private void OnDrawGizmosSelected()
     {
