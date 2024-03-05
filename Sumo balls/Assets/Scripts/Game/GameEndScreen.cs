f6d8e399762ceff5dea9aaf71b9789269a5844c6
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-03-05 19:53:32 +0100

    Set cursor visible when stage is ended.

--- Sumo balls/Assets/Scripts/Game/GameEndScreen.cs
+++ Sumo balls/Assets/Scripts/Game/GameEndScreen.cs
@@ -7,11 +7,13 @@ public class GameEndScreen : MonoBehaviour
 {
     [SerializeField] GameObject _gameOverScreen;
     [SerializeField] GameObject _stageClearScreen;
+    [SerializeField] CursorLockControl _lockControl;
     public void ShowGameOverScreen()
     {
         gameObject.SetActive(true);
         _gameOverScreen.SetActive(true);
         _stageClearScreen.SetActive(false);
+        _lockControl.SetCursorVisibilty(true);
     }
 
     public void ShowStageClearScreen()
@@ -19,9 +21,11 @@ public class GameEndScreen : MonoBehaviour
         gameObject.SetActive(true);
         _gameOverScreen.SetActive(false);
         _stageClearScreen.SetActive(true);
+        _lockControl.SetCursorVisibilty(true);
     }
     public void Hide()
     {
         gameObject.SetActive(false);
+        _lockControl.SetCursorVisibilty(false);
     }
 }
