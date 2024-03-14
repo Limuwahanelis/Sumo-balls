ce6156e66f021f02805615348152e1fcb3ea63c2
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-03-14 13:44:03 +0100

    Update HoleInArenaSpawner.cs
    Add buttons to test spawner.
    Add check to prevent spawning holes on each other.

--- Sumo balls/Assets/Scripts/Enemy/Traps/HoleInArena/HoleInArenaSpawner.cs
+++ Sumo balls/Assets/Scripts/Enemy/Traps/HoleInArena/HoleInArenaSpawner.cs
@@ -17,15 +17,6 @@ public class HoleInArenaSpawner : MonoBehaviour
     [SerializeField] TimeCounter _timeCounter;
     [SerializeField] bool _debug;
     [SerializeField,HideInInspector] HoleInArenaSettings _settings;
-    //[SerializeField] float _arenaHeight;
-    //[SerializeField] float _firstHoleDelay;
-    //[SerializeField] float _minHoleMaxRadius;
-    //[SerializeField] float _maxHoleMaxRadius;
-    //[SerializeField] float _timeToReachMaxzRadius;
-    //[SerializeField] float _timeToStayAtMaxRadius;
-    //[SerializeField] float _timeToBeginGrowth;
-    //[SerializeField] float _spawnDelay;
-    //[SerializeField] int _numberOfConcurrentHoles;
 
 
     private List<HoleInArenaManager> _allHolesInArena = new List<HoleInArenaManager>();
@@ -33,6 +24,7 @@ public class HoleInArenaSpawner : MonoBehaviour
     private List<Spawncandidate> _candidatesToSpawn = new List<Spawncandidate>();
 
     private bool _isSpawningHoles;
+    [SerializeField, HideInInspector] private float _testRadius;
     [SerializeField,HideInInspector] private bool _isSetUp;
     // Start is called before the first frame update
     void Start()
@@ -56,10 +48,15 @@ public class HoleInArenaSpawner : MonoBehaviour
     }
     private void SpawnHole()
     {
-        HoleInArenaManager hole = _pool.GetHole();
 
         float maxHoleRadius = Random.Range(_settings.MinMaxHoleRadius, _settings.MaxHoleMaxRadius);
-        Vector3 holeSpawnPos = SelectSpawnPos(maxHoleRadius);
+        Vector3 holeSpawnPos = SelectSpawnPos(maxHoleRadius,out bool canSpawn);
+        if(!canSpawn) // if hole can't be safetly spawned, abort.
+        {
+            _timeCounter.ResetTimer();
+            return;
+        }
+        HoleInArenaManager hole = _pool.GetHole();
         holeSpawnPos.y = _settings.ArenaHeight;
         hole.transform.position = holeSpawnPos;
         hole.SetUpHole(maxHoleRadius, _settings.TimeToReachMaxRadius, _settings.TimeToStayAtMaxRadius, _settings.TimeToBeginGrowth);
@@ -83,25 +80,16 @@ public class HoleInArenaSpawner : MonoBehaviour
     }
     public void SetSpawnParameters(HoleInArenaSettings settings)
     {
-        //_arenaHeight = settings.ArenaHeight;
-        //_minHoleMaxRadius = settings.MinMaxHoleRadius;
-        //_maxHoleMaxRadius = settings.MaxHoleMaxRadius;
-        //_timeToReachMaxzRadius = settings.TimeToReachMaxRadius;
-        //_numberOfConcurrentHoles = settings.NumberOfConcurrentHoles;
-        //_timeToStayAtMaxRadius = settings.TimeToStayAtMaxRadius;
-        //_timeToBeginGrowth = settings.TimeToBeginGrowth;
-        //_spawnDelay = settings.SpawnDelay;
         _settings = settings;
         _isSetUp = true;
     }
-    private Vector3 SelectSpawnPos(float holeRadius)
+    private Vector3 SelectSpawnPos(float holeRadius, out bool canSpawn)
     {
         _candidatesToSpawn.Clear();
-        Vector3 enemySpawnPos = _hexagonSpawn.GetAvilablePosition(holeRadius * 2 * math.sqrt(2), holeRadius * 2 * math.sqrt(2));
+        Vector3 enemySpawnPos = _hexagonSpawn.GetAvilablePositionForSphereInside(holeRadius);
         bool isSelectingSpawnpos = true;
         int selectiontries = 1;
-        bool canSpawn = true;
-        Spawncandidate winner;
+        canSpawn = true;
         while (isSelectingSpawnpos)
         {
             canSpawn = true;
@@ -124,27 +112,14 @@ public class HoleInArenaSpawner : MonoBehaviour
             if (!canSpawn)
             {
                 selectiontries++;
-                enemySpawnPos = _hexagonSpawn.GetAvilablePosition(holeRadius * 2 * math.sqrt(2), holeRadius * 2 * math.sqrt(2));
+                enemySpawnPos = _hexagonSpawn.GetAvilablePositionForSphereInside(holeRadius);
 
                 if (selectiontries == 10) isSelectingSpawnpos = false;
             }
             else isSelectingSpawnpos = false;
         }
         if (canSpawn) return _candidatesToSpawn[_candidatesToSpawn.Count - 1].position;
-        else
-        {
-            winner = _candidatesToSpawn[0];
-            winner.distances.Sort();
-            for (int i = 1; i < _candidatesToSpawn.Count; i++)
-            {
-                _candidatesToSpawn[i].distances.Sort();
-                if (_candidatesToSpawn[i].distances[0] > winner.distances[0])
-                {
-                    winner = _candidatesToSpawn[i];
-                }
-            }
-        }
-        return winner.position;
+        else return Vector3.zero;
     }
     public void ClearSpawn()
     {
@@ -161,26 +136,40 @@ public class HoleInArenaSpawner : MonoBehaviour
         _enemySpawner.UpdateAvoidTransforms(hole.transform, hole.MaxRadius, true,true);
         _holesInArena.Remove(hole);
     }
-    private void TestSpawn()
+    private void TestSingleSpawn()
     {
-        float maxHoleRadius = Random.Range(_settings.MinMaxHoleRadius, _settings.MaxHoleMaxRadius);
-        Debug.Log(string.Format("radius: {0}",maxHoleRadius));
-        _timeCounter.ResetTimer();
-        _debugSphere.transform.localScale = new Vector3(maxHoleRadius*2, maxHoleRadius*2, maxHoleRadius*2);   
-        for (int i= 0; i < 60; i++)
-            Instantiate(_debugSphere, _hexagonSpawn.GetAvilablePosition(maxHoleRadius, maxHoleRadius), _debugSphere.transform.rotation);
+        //2.183955
+        float maxHoleRadius = _testRadius; //Random.Range(_settings.MinMaxHoleRadius, _settings.MaxHoleMaxRadius);
+        _debugSphere.transform.localScale = new Vector3(maxHoleRadius*2, maxHoleRadius*2, maxHoleRadius*2);
+        for (int i = 0; i < 60; i++)
+        {
+            GameObject go = Instantiate(_debugSphere, _hexagonSpawn.GetAvilablePositionForSphereInside(maxHoleRadius), _debugSphere.transform.rotation);
+            go.transform.position = new Vector3(go.transform.position.x, _settings.ArenaHeight, go.transform.position.z);
+        }
+    }
+    private void TestSettingseSpawn()
+    {
+        float maxHoleRadius = Random.Range(_settings.MinMaxHoleRadius,_settings.MaxHoleMaxRadius); //Random.Range(_settings.MinMaxHoleRadius, _settings.MaxHoleMaxRadius);
+        _debugSphere.transform.localScale = new Vector3(maxHoleRadius * 2, maxHoleRadius * 2, maxHoleRadius * 2);
+        for (int i = 0; i < 60; i++)
+        {
+            GameObject go = Instantiate(_debugSphere, _hexagonSpawn.GetAvilablePositionForSphereInside(maxHoleRadius), _debugSphere.transform.rotation);
+            go.transform.position = new Vector3(go.transform.position.x, _settings.ArenaHeight, go.transform.position.z);
+        }
     }
 #if UNITY_EDITOR
     [CustomEditor(typeof(HoleInArenaSpawner))]
     public class HoleInArenaSpawnerEditor: Editor
     {
-        SerializedProperty holeSettings;
+        SerializedProperty settings;
+        SerializedProperty testRadius;
         SerializedProperty debug;
         SerializedProperty isSetUp;
         SerializedProperty debugSphere;
         private void OnEnable()
         {
-            holeSettings = serializedObject.FindProperty("_settings");
+            testRadius = serializedObject.FindProperty("_testRadius");
+            settings = serializedObject.FindProperty("_settings");
             debug = serializedObject.FindProperty("_debug");
             isSetUp = serializedObject.FindProperty("_isSetUp");
             debugSphere = serializedObject.FindProperty("_debugSphere");
@@ -192,11 +181,16 @@ public class HoleInArenaSpawner : MonoBehaviour
             if(debug.boolValue)
             {
                 isSetUp.boolValue = true;
-                EditorGUILayout.PropertyField(holeSettings);
+                EditorGUILayout.PropertyField(testRadius);
+                EditorGUILayout.PropertyField(settings);
                 EditorGUILayout.PropertyField(debugSphere);
-                if (GUILayout.Button("Test Spawn"))
+                if (GUILayout.Button("Test single Spawn"))
                 {
-                    (target as HoleInArenaSpawner).TestSpawn();
+                    (target as HoleInArenaSpawner).TestSingleSpawn();
+                }
+                if (GUILayout.Button("Test settings spawn"))
+                {
+                    (target as HoleInArenaSpawner).TestSettingseSpawn();
                 }
             }
             else
