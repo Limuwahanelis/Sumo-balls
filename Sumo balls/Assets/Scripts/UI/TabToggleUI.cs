19818d066495a881f9db0f5c7bd5b4dbaf4fbadd
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-04-09 23:31:51 +0200

    Add new tabs based on toggle.

---
+++ Sumo balls/Assets/Scripts/UI/TabToggleUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabToggleUI : Toggle
+{
+    private Graphic _startingtargetGraphic;
+    protected override void Awake()
+    {
+        base.Awake();
+        _startingtargetGraphic = targetGraphic;
+        
+    }
+    public void SwapTargetGraphic(bool isOn)
+    {
+        if (isOn) targetGraphic = graphic;
+        else targetGraphic = _startingtargetGraphic;
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (isOn) return;
+        else base.OnPointerDown(eventData);
+    }
+    public override void OnSubmit(BaseEventData eventData)
+    {
+        if (isOn) return;
+        base.OnSubmit(eventData);
+    }
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (isOn) return;
+        base.OnPointerClick(eventData);
+    }
+}
