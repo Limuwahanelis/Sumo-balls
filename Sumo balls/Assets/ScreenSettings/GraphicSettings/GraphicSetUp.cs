e5759a8f0084ab11c6d6848f2c4f87d5bad76a70
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-06-03 22:03:29 +0200

    Add script to set up graphics at start

---
+++ Sumo balls/Assets/ScreenSettings/GraphicSettings/GraphicSetUp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicSetUp : MonoBehaviour
+{
+    private void Awake()
+    {
+        GraphicSettingsData data = GraphicSettingsSaver.LoadGraphicSettings();
+        if (data==null)
+        {
+            data = new GraphicSettingsData(2);
+            GraphicSettingsSaver.SaveGraphicSettings(data);
+            QualitySettings.SetQualityLevel(data.qualitySettingsIndex);
+        }
+        else
+        {
+
+        }
+    }
+}
